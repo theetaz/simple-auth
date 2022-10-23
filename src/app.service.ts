24bae3f7b4c183c1c293993ee58212f49074b810
Nipun Theekshana <theekshana2@gmail.com>
2022-10-24 00:40:27 +0530

    chore: change welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { message: "Welcome to the kingsfund dex platform version 1.0v" };
+    return { message: "Welcome to the simple auth backend application 1.0v" };
   }
 }
